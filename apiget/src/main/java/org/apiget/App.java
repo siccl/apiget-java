36cbc870d32e6209f80bf76265c13bdcd06b8d1a
Hansel Stange Gaete <hansel.stange@sic.cl>
2023-01-30 10:57:03 -0300

    clean code

--- apiget/src/main/java/org/apiget/App.java
+++ apiget/src/main/java/org/apiget/App.java
@@ -1,6 +1,5 @@
 package org.apiget;
 
-import org.apache.commons.codec.digest.DigestUtils;
 import io.github.cdimascio.dotenv.Dotenv;
 import java.util.Date;
 import io.jsonwebtoken.Jwts;
@@ -16,25 +15,25 @@ import java.nio.charset.StandardCharsets;
 
 public class App 
 {
-    // internal properties
     private String APIKEY;
     private String APISECRET;
     private String APIURL;
     private String JWTTOKEN;
-
+    // main method
+    // read .env file
+    // generate JWT token
+    // call API
+    // print response
+    // some debug info
     public static void main(String[] args) throws IOException {
 
         App Obj = new App();
         Obj.readEnv(Obj);
-        // generate JWT token
         Obj.JWTTOKEN = Obj.generateJWT(Obj);
         System.out.println(Obj.JWTTOKEN);
-
         URL url;
         try {
-
             url = new URL(Obj.APIURL + "?a=" + Obj.GenerateRandomNumber() + "&b=" + Obj.GenerateRandomNumber());
-            // print url
             System.out.println(url);
             HttpURLConnection con = (HttpURLConnection) url.openConnection();
             con.setRequestProperty("Content-Type", "application/json");
@@ -42,39 +41,27 @@ public class App
             con.setRequestMethod("GET");
             con.setDoOutput(true);
             con.setDoInput(true);
-            // read response
             int status = con.getResponseCode();
             System.out.println(status);
             InputStream inputStream = con.getInputStream();
-            //System.out.println(con.getResponseMessage());
             ByteArrayOutputStream result = new ByteArrayOutputStream();
             byte[] buffer = new byte[1024];
             for (int length; (length = inputStream.read(buffer)) != -1; ) {
                 result.write(buffer, 0, length);
             }
-            // StandardCharsets.UTF_8.name() > JDK 7
             System.out.println(result.toString("UTF-8"));
             con.disconnect();
 
         } catch (MalformedURLException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         }
     }
-
-    public static String sha256hex(String input) {
-        return DigestUtils.sha256Hex(input);
-    }
-
     // read .env file
     public void readEnv(App Obj) {
          try {
              Dotenv dotenv = Dotenv.load();
-                // read and set TOKEN/APISECRET
                 Obj.APIKEY = dotenv.get("APIKEY");
-                //Obj.APISECRET = dotenv.get("TOKEN");
                 Obj.APISECRET = dotenv.get("TOKEN");
-                //get("APISECRET");
                 Obj.APIURL = dotenv.get("API");
 
          } catch (Exception e) {
@@ -83,12 +70,9 @@ public class App
     }
     // Generate JWT token
     public String generateJWT(App Obj) {
-        // JWT token
         String token = "";
-        //SecretKey key = Keys.hmacShaKeyFor(Decoders.BASE64.decode(Obj.APISECRET));
         SecretKey key = Keys.hmacShaKeyFor(Obj.APISECRET.getBytes(StandardCharsets.UTF_8));
         try {
-
             token = Jwts.builder()
             .setIssuer(Obj.APIKEY)
             .setIssuedAt(new Date())
@@ -97,16 +81,14 @@ public class App
             .compact();
 
         } catch (Exception e) {
-            // show complete error trace
             System.out.println(e);
-
         }
         return token;
     }
-    // generate GenerateRandomNumber method
+    // Generate random number between 1 and 100
     public String GenerateRandomNumber() {
-        int min = 50; // Minimum value of range
-        int max = 100; // Maximum value of range
+        int min = 1;
+        int max = 100;
         int random_int = (int)Math.floor(Math.random() * (max - min + 1) + min);
         return Integer.toString(random_int);
     }
